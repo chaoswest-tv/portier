f97202869c9f244b87ffa7f30d1f09b64b1d11aa
Jan Koppe <post@jankoppe.de>
2020-04-20 18:56:12 +0200

    fix celery settings

--- portier/celery.py
+++ portier/celery.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
-app = Celery('proj')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portier.settings')
+app = Celery('portier')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
