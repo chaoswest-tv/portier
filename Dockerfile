805e2911f8605e64e7ab464a2d564b4f0603523a
Jan Koppe <post@jankoppe.de>
2020-04-25 10:22:22 +0200

    run nginx and gunicorn with supervisord in container

--- Dockerfile
+++ Dockerfile
@@ -4,23 +4,24 @@ WORKDIR /app
 # set env
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-EXPOSE 8000
 
-RUN apk update \
-    && apk add postgresql-dev gcc python3-dev musl-dev gettext postgresql-client
+# install required packages
+RUN apk add --no-cache postgresql-dev gcc python3-dev musl-dev gettext postgresql-client nginx supervisor
 
 # install dependencies
 ADD ./requirements.txt .
 RUN pip install -r requirements.txt
+
+# add supervisor and nginx configs
+ADD ./docker/nginx.conf /etc/nginx/nginx.conf
+ADD ./docker/supervisord.conf /etc/supervisord.conf
 
 # add user
 RUN addgroup -S portier && adduser -S portier -G portier
 
 # add code
 ADD --chown=portier:portier . /app
-
 RUN ./manage.py collectstatic --noinput --link
 RUN ./manage.py compilemessages
-USER portier
 
 CMD ["/app/start.sh", "migrate_start"]
--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-migrate() {
-  python manage.py makemigrations
-  python manage.py migrate
-}
-
 wait_for_redis() {
   echo "Trying to connect to redis at ${REDIS_HOST:-redis}:${REDIS_PORT:-6379} ..."
   while ! nc -z ${REDIS_HOST:-redis} ${REDIS_PORT:-6379};
@@ -23,21 +18,12 @@ wait_for_database() {
   echo "Successfully connected to database. Continuing.";
 }
 
-case $1 in
-  "create_superuser" )
-    wait_for_redis
-    wait_for_database
-    python manage.py createsuperuser --no-input --username "${ADMIN_USER:-admin}" --email "${ADMIN_EMAIL:-post@chaoswest.tv}"
-    ;;
-  "migrate_start" )
-    wait_for_redis
-    wait_for_database
-    migrate
-    gunicorn -w 4 --bind 0.0.0.0:${EXPOSE_PORT:-8000} portier.wsgi
-    ;;
-  "only_start" )
-    wait_for_redis
-    wait_for_database
-    gunicorn -w 4 --bind 0.0.0.0:${EXPOSE_PORT:-8000} portier.wsgi
-    ;;
-esac
+migrate() {
+  python manage.py makemigrations
+  python manage.py migrate
+}
+
+wait_for_redis
+wait_for_database
+migrate
+supervisord -n -c /etc/supervisord.conf
